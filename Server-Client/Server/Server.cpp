d8095928a44f9a5badf0191269b9aa23d8642d41
Dina Nashaat <dina.nashaat@gmail.com>
2016-11-05 21:29:35 +0200

    Add server-client connection

--- Server-Client/Server/Server.cpp
+++ Server-Client/Server/Server.cpp
@@ -24,4 +24,33 @@ int main()
 		MessageBoxA(NULL, "WinSock startup failed", "Error", MB_OK | MB_ICONERROR);
 		return 0;
 	}
+	//Setup the Address
+	SOCKADDR_IN address;
+	address.sin_addr.s_addr = inet_addr("192.168.1.100");
+	address.sin_family = AF_INET;
+	address.sin_port = htons(1111);
+	int addressLength = sizeof(address);
+
+
+	//Listen for Connections
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
+	bind(sListen, (SOCKADDR*)&address, sizeof(address));
+	listen(sListen, SOMAXCONN);
+
+	//Parse HTTP/1.0 Request
+	array<string, 3> clientParams;
+	char buffer[] = "You are now connected to Dina's Server";
+
+	//Hold Client Connection	
+	SOCKET conn;
+	conn = accept(sListen, (SOCKADDR*)&address, &addressLength);
+	if (conn == 0)
+	{
+		cout << "Connection Failed" << endl;
+	}
+	else
+	{
+		cout << "Client Connected" << endl;
+
+	}
 }
