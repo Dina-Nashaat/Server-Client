6775ed5385d6618a9aba84f8b2bdf09b7569b31e
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-11-05 19:36:30 +0200

    Recive and Send using Client

--- Server-Client/Client/Client.cpp
+++ Server-Client/Client/Client.cpp
@@ -10,11 +10,40 @@
 
 int main()
 {
-	//WinSock Startup
+	//Winsock Startup
 	WSAData wsaData;
 	WORD DllVersion = MAKEWORD(2, 1);
 	if (WSAStartup(DllVersion, &wsaData) != 0) //If WSAStartup returns anything other than 0, then that means an error has occured in the WinSock Startup.
 	{
-		MessageBoxA(NULL, "WinSock startup failed", "Error", MB_OK | MB_ICONERROR);
-		return 0;
+		MessageBoxA(NULL, "Winsock startup failed", "Error", MB_OK | MB_ICONERROR);
+		exit(1);
 	}
+
+	SOCKADDR_IN addr; //Address to be binded to our Connection socket
+	int sizeofaddr = sizeof(addr); //Need sizeofaddr for the connect function
+	addr.sin_addr.s_addr = inet_addr("192.168.1.100"); //Address = localhost (this pc)
+	addr.sin_port = htons(1111); //Port = 1111
+	addr.sin_family = AF_INET; //IPv4 Socket
+
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL); //Set Connection socket
+	if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) //If we are unable to connect...
+	{
+		MessageBoxA(NULL, "Failed to Connect", "Error", MB_OK | MB_ICONERROR);
+		return 0; //Failed to Connect
+	}
+	std::cout << "Connected!" << std::endl;
+
+	char MOTD[256];
+	recv(Connection, MOTD, sizeof(MOTD), NULL); //Receive Message of the Day buffer into MOTD array
+	std::cout << "MOTD:" << MOTD << std::endl;
+
+	char MOTD2[] = "GET oop.txt hostname";
+	send(Connection, MOTD2, sizeof(MOTD2), NULL);
+	recv(Connection, MOTD2, sizeof(MOTD), NULL); //Receive Message of the Day buffer into MOTD array
+	std::cout << MOTD2;
+
+	while (true)
+	{
+		Sleep(10);
+	}
+}	
