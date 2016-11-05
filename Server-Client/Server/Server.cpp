1aa6178d13b8f5220211c5151257a3dbe2c8edf0
Dina Nashaat <dina.nashaat@gmail.com>
2016-11-05 21:25:36 +0200

    Add parseRequest

--- Server-Client/Server/Server.cpp
+++ Server-Client/Server/Server.cpp
@@ -5,8 +5,14 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #include <WinSock2.h>
 #include <iostream>
+#include "stdlib.h"
 #pragma comment(lib,"ws2_32.lib")
+#include <array>
+#include <string>
+#include "helper.h"
 
+
+using namespace std;
 
 int main()
 {
---
+++ Server-Client/Server/helper.h
@@ -0,0 +1,30 @@
+#pragma once
+
+
+
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <array>
+#include <fstream>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+
+array<string, 3> parseRequest(string request)
+{
+	array<string, 3> requestParams;
+	stringstream ss(request);
+	string token;
+
+	int i = 0;
+	while (getline(ss, token, ' '))
+	{
+		requestParams[i] = token;
+		i++;
+	}
+	return requestParams;
+}
+
