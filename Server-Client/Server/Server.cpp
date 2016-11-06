86dfaf1373b1d59efb8cdb29f23411fef06e71a7
Dina Nashaat <dina.nashaat@gmail.com>
2016-11-07 00:02:20 +0200

    Loop server accept connection forever

--- Server-Client/Server/Server.cpp
+++ Server-Client/Server/Server.cpp
@@ -24,74 +24,77 @@ int main()
 		MessageBoxA(NULL, "WinSock startup failed", "Error", MB_OK | MB_ICONERROR);
 		return 0;
 	}
-	//Setup the Address
-	SOCKADDR_IN address;
-	address.sin_addr.s_addr = inet_addr("192.168.1.100");
-	address.sin_family = AF_INET;
-	address.sin_port = htons(1111);
-	int addressLength = sizeof(address);
+	
+	/*Setup the Address clients will connect to*/
+	SOCKADDR_IN address; //Initialize instance of address
+	address.sin_addr.s_addr = inet_addr("192.168.1.100"); //specify the address clients connect to
+	address.sin_family = AF_INET; //address follows ipv4 
+	address.sin_port = htons(1111); //convert port number to network byte order (htons)
+	int addressLength = sizeof(address); //used later bind() and accept() system calls
 
 
-	//Listen for Connections
-	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
-	bind(sListen, (SOCKADDR*)&address, sizeof(address));
+	/*Listen for incoming Connections*/
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); //Specify socket to listen at
+	bind(sListen, (SOCKADDR*)&address, addressLength); 
 	listen(sListen, SOMAXCONN);
 
-	//Parse HTTP/1.0 Request
+	/*Message Instances*/
 	array<string, 3> clientParams;
-	char buffer[] = "You are now connected to Dina's Server";
+	char buffer[] = "You are now connected to Dina's Server"; //Message from server to client.
+	char request[4096]; //Receive request messages from client to server
+	string requestCommand; //GET or POST
+	string filename;
+	string hostname; //Server Name
+	int exists;
 
 	//Hold Client Connection	
 	SOCKET conn;
-	conn = accept(sListen, (SOCKADDR*)&address, &addressLength);
-	if (conn == 0)
+	while (1)
 	{
-		cout << "Connection Failed" << endl;
-	}
-	else
-	{
-		cout << "Client Connected" << endl;
-		send(conn, buffer, sizeof(buffer), NULL); //Send Connection Success Message
-
-		char request[4096];
-		recv(conn, request, sizeof(request), NULL); //Receive text request from Client
-		cout << request << endl;
-
-		clientParams = parseRequest(request);
-		string requestCommand;
-		string filename;
-		string hostname;
-
-
-		clientParams = parseRequest(request); //Parse request incoming from client [0]GET/POST
-		requestCommand = clientParams[0];
-		filename = clientParams[1];
-		hostname = clientParams[2];
-
-		int exists;
-		if (requestCommand == "GET")
+		conn = accept(sListen, (SOCKADDR*)&address, &addressLength); //Wait for incoming connection from clients
+		if (conn == 0)
 		{
-			//VALIDATE IF FILE EXISTS
-			exists = readFile(filename, buffer);
-			if (!exists)
-			{
-				char buffer[] = "file doesn't exist";
-				cout << "file doesn't exist";
-				//return 0;
-			}
-			else
-			{
-				//READ FILE FROM DISK
-				send(conn, buffer, sizeof(buffer), NULL); //Send Connection Success Message
-				cout << "File sent succesfully" << endl;
-				//CLOSE CONNECTION
-			}
+			cout << "Connection Failed" << endl;
 		}
-		else if (requestCommand == "POST")
+		else
 		{
-			recv(conn, request, sizeof(request), NULL);
-			//SAVE FILE INTO DISK
-			cout << "File has been uploaded to server.";
+			cout << "Client Connected" << endl;
+			send(conn, buffer, sizeof(buffer), NULL); //Send Connection Success Message
+
+			
+			recv(conn, request, sizeof(request), NULL); //Receive text request from Client
+			cout << request << endl; //Print out recieved command. 
+
+			/*Parse client request (GET or POST)*/
+			clientParams = parseRequest(request); //Parse request incoming from client [0]GET/POST
+			requestCommand = clientParams[0];
+			filename = clientParams[1];
+			hostname = clientParams[2];
+
+			if (requestCommand == "GET")
+			{
+				//VALIDATE IF FILE EXISTS
+				exists = readFile(filename, buffer); //read file returns 1 if file found and inserts into buffer the data of the file. 
+				if (!exists)
+				{
+					char buffer[] = "HTTP/1.0 404 Not Found \r\n";
+					cout << "File requested by client does not exist";
+					send(conn, buffer, sizeof(buffer), NULL);
+				}
+				else
+				{
+					//READ FILE FROM DISK
+					send(conn, buffer, sizeof(buffer), NULL); //Send Connection Success Message
+					cout << "File sent succesfully" << endl;
+					closesocket(conn);
+				}
+			}
+			else if (requestCommand == "POST")
+			{
+				recv(conn, request, sizeof(request), NULL);
+				//SAVE FILE INTO DISK
+				cout << "File has been uploaded to server.";
+			}
 		}
 
 	}
